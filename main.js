c2de36ccec8bda58736b5651ae1b325a5c6c3289
Skalex Stuch <97718450+mussino@users.noreply.github.com>
2023-04-25 05:21:33 +0000

    update

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const hexInput = document.getElementById('hexInput');
+const inputColor = document.getElementById('inputColor');
+
+
+hexInput.addEventListener('keyup', () => {
+    const hex = hexInput.value;
+    if (!isValidHex(hex)) return;
+
+    const strippedHex = hex.replace('#', '');
+    inputColor.style.backgroundColor = '#' + strippedHex;
+})
+
+
 const isValidHex = (hex) => {
     if (!hex) return false;
 
@@ -5,9 +18,27 @@ const isValidHex = (hex) => {
     return strippedHex.length === 3 || strippedHex.length === 6;
 }
 
-/*console.log(isValidHex('#000'));
-console.log(isValidHex('#000000'));
-console.log(isValidHex('#0000000'));
-console.log(isValidHex('#0007rhju63'));
-*/
+const convertHexToRGB = (hex) => {
+    if (!isValidHex(hex)) return null;
 
+    let strippedHex = hex.replace('#', '');
+
+    if (strippedHex.length === 3) {
+        strippedHex = strippedHex[0] + strippedHex[0] + strippedHex[1] + strippedHex[1] + strippedHex[2] + strippedHex[2];
+    }
+    const r = parseInt(strippedHex.substring(0,2), 16);
+    const g = parseInt(strippedHex.substring(2,4), 16);
+    const b = parseInt(strippedHex.substring(4,6), 16);
+
+    return {r,g,b};
+}
+
+
+const convertRGBToHex = (r,g,b) => {
+    const firstPair = r.toString(16);
+    const secondPair = g.toString(16);
+    const thirdPair = b.toString(16);
+
+    const hex = '#' + firstPair + secondPair + thirdPair;
+    return hex;
+}
